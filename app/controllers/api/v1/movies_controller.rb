f05371e273bc00623588de19f7b2b060b6254352
Roman Pirag <romanpirag@gmail.com>
2019-08-01 15:49:50 -0400

    Adds index and show to Movies Controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::MoviesController < ApplicationController
+
+    def index
+        @movies = Movie.all.order("id ASC").all
+        render json: @movies
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+        render json: @movie
+    end
 end
