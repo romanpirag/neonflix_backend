e0abaa920a146da9cc4567f46f9ee5d57fbe3b21
Roman Pirag <romanpirag@gmail.com>
2019-08-01 22:48:41 -0400

    Adds director to Movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_195814) do
+ActiveRecord::Schema.define(version: 2019_08_02_024601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_01_195814) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all 
+
+# Fantasty::
+neverending_story = Movie.create(title: "The Neverending Story", genre: "fantasy", year: 1984, cast: ["Barret Oliver", "Noah Hathaway", "Tami Stronach"], rating: 9, image: "https://i.imgur.com/m693bPT.jpg", synopsis: "A troubled boy dives into a wondrous fantasy world through the pages of a mysterious book." )
+labyrinth = Movie.create(title: "Labyrinth", genre: "fantasy", year: 1986, cast: ["David Bowie", "Jennifer Connelly", "Toby Froud"], rating: 7, image: "https://i.imgur.com/m693bPT.jpg", synopsis: "A 16-year-old girl is given 13 hours to solve a labyrinth and rescue her baby brother when her wish for him to be taken away is granted by the Goblin King."  )
+the_dark_crystal = Movie.create(title: "The Dark Crystal", genre: "fantasy", year: 1982, cast: ["Jim Henson", "Frank Oz", "Dave Goelz"], rating: 6, image: "https://i.imgur.com/m693bPT.jpg", synopsis: "On another planet in the distant past, a Gelfling embarks on a quest to find the missing shard of a magical crystal, and so restore order to his world."  )
+legend = Movie.create(title: "Lengend", genre: "fantasy", year: 1985, cast: ["Tom Cruise", "Mia Sara", "Tim Curry"], rating: 7, image: "https://i.imgur.com/m693bPT.jpg", synopsis: "A young man must stop the Lord of Darkness from both destroying daylight and marrying the woman he loves."  )
+
+
+# Horror::
+
+
+# Action::
+
+
+# Drama::
+
+
+# Toons:: 
+
+
+# Comedy:: 
+
+
+# Romance:: 
+
+
+# Scifi
+
+
+
+
+puts "SEEDED!!✨ ⭐️ 🌈 🐶 ✨"
